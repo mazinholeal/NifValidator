44f3fed12bc76e44e7f942cf2209418051cd0984
mazinholeal <osmar_maciel@hotmail.com>
2025-11-03 12:32:24 -0300

    Update test_nif_validator.py

--- tests/test_nif_validator.py
+++ tests/test_nif_validator.py
@@ -1,20 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Valida NIF</title>
-</head>
-<body>
-    <h1>NIF Validator - cfreire</h1>
-    <hr>
-    <form method="POST">
-        <input name="text", placeholder="NIF">
-        <input type="submit">
-    </form>
+import nif_validator as validator
 
-    {{ result }}
-    
-</body>
-</html>
+
+def test_positive():
+    db = ['292849680', '245832343', '121588955', '592889050', '522523129', '620365498', 
+          '636695468', '998113360']
+    for nif in db:
+        print('[+] nif: {}'.format(nif))
+        assert validator.valida_nif(nif)
+
+
+def test_negative():
+    db = ['292849681', '245382343', '12158895', '59288905000', '999999999', '123', '639566468']
+    for nif in db:
+        print('[-] nif: {}'.format(nif))
+        assert validator.valida_nif(nif) == False
+
+
+def test_dummy():
+    db = ['', 'abc', '\r\n', '999999abc', '9999O9999', '999999990abc', 'a12345678', 
+          'username!', '@999999990',
+          ' 999999990', '999999990 ']
+    for nif in db:
+        print('[*] nif: {}'.format(nif))
+        assert validator.valida_nif(nif) == False
